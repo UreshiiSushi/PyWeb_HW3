03e30a3be74ab91b10c8ef009cb9c4c2fcaafaaa
UreshiiSushi <merieru@gmail.com>
2024-03-18 02:46:17 +0100

    Add timestamps for simple factorize

--- factorize.py
+++ factorize.py
@@ -1,4 +1,6 @@
-def factorize(*number):
+from time import time
+
+def simple_factorize(*number):
     result = []
     for n in number:
         dividers = []
@@ -14,9 +16,14 @@ def factorize(*number):
 
 if __name__ == '__main__':
     # These "asserts" using only for self-checking and not necessary for auto-testing
-    a, b, c, d  = factorize(128, 255, 99999, 10651060)
-
+    start = time()
+    a, b, c, d  = simple_factorize(128, 255, 99999, 10651060)
     assert a == [1, 2, 4, 8, 16, 32, 64, 128]
     assert b == [1, 3, 5, 15, 17, 51, 85, 255]
     assert c == [1, 3, 9, 41, 123, 271, 369, 813, 2439, 11111, 33333, 99999]
     assert d == [1, 2, 4, 5, 7, 10, 14, 20, 28, 35, 70, 140, 76079, 152158, 304316, 380395, 532553, 760790, 1065106, 1521580, 2130212, 2662765, 5325530, 10651060]
+
+    finish = time
+    print(f"Factorizing without multiprocessing took: {finish - start} sec")
+
+    
