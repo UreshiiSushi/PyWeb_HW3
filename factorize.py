e8f8bd2ccb5ca7665561139a93f5960c6a40f20b
UreshiiSushi <merieru@gmail.com>
2024-03-18 03:36:47 +0100

    Multiprocessing realisation

--- factorize.py
+++ factorize.py
@@ -1,4 +1,10 @@
+from pprint import pprint
 from time import time
+from multiprocessing import Process, cpu_count
+
+
+
+
 
 def simple_factorize(*number):
     result = []
@@ -12,6 +18,14 @@ def simple_factorize(*number):
         result.append(dividers)
     return result
 
+def multiproc_factorize(number):
+    dividers = []
+    divider = 1
+    while divider <= number:
+        if number % divider == 0:
+            dividers.append(divider)
+        divider += 1
+    return dividers
 
 
 if __name__ == '__main__':
@@ -23,7 +37,23 @@ if __name__ == '__main__':
     assert c == [1, 3, 9, 41, 123, 271, 369, 813, 2439, 11111, 33333, 99999]
     assert d == [1, 2, 4, 5, 7, 10, 14, 20, 28, 35, 70, 140, 76079, 152158, 304316, 380395, 532553, 760790, 1065106, 1521580, 2130212, 2662765, 5325530, 10651060]
 
-    finish = time
+    finish = time()
     print(f"Factorizing without multiprocessing took: {finish - start} sec")
 
+    print(f"Number of CPU: {cpu_count()}")
+
+    start = time()
+    processes = []
+    for i in (128, 255, 99999, 10651060):
+        p = Process(target=multiproc_factorize, args=(i,))
+        p.start()
+        processes.append(p)
+    finish = time()
+    [p.join() for p in processes]
+    # a, b, c, d = 
+    # assert a == [1, 2, 4, 8, 16, 32, 64, 128]
+    # assert b == [1, 3, 5, 15, 17, 51, 85, 255]
+    # assert c == [1, 3, 9, 41, 123, 271, 369, 813, 2439, 11111, 33333, 99999]
+    # assert d == [1, 2, 4, 5, 7, 10, 14, 20, 28, 35, 70, 140, 76079, 152158, 304316, 380395, 532553, 760790, 1065106, 1521580, 2130212, 2662765, 5325530, 10651060]
     
+    print(f"Factorizing with multiprocessing took: {finish - start} sec")
